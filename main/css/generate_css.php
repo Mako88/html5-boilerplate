00b82979b5d131aaafe06c79f99ba9767b7dbc6f
BuildTools <afuturepilotis@gmail.com>
2016-12-13 23:09:14 -0600

    Use <cms:abort /> to remove css comment

--- main/css/generate_css.php
+++ main/css/generate_css.php
@@ -1,14 +1,16 @@
 <?php require_once( '../couch/cms.php' ); ?>
-<cms:content_type 'text/css' />
-<cms:template title="CSS Generator" hidden='1' />
+<cms:abort>
+    <cms:content_type 'text/css' />
+    <cms:template title="CSS Generator" hidden='1' />
 
-<cms:embed '../../css/normalize.css' />
-<cms:embed '../../css/defaults.css' />
-<cms:embed '../../css/fonts.css' />
-<cms:embed '../../css/menu.css' />
-<cms:embed '../../css/form.css' />
-<cms:embed '../../css/blog.css' />
-<cms:embed '../../css/styles.css' />
-<cms:embed '../../css/responsive.css' />
+    <cms:embed '../../css/normalize.css' />
+    <cms:embed '../../css/defaults.css' />
+    <cms:embed '../../css/fonts.css' />
+    <cms:embed '../../css/menu.css' />
+    <cms:embed '../../css/form.css' />
+    <cms:embed '../../css/blog.css' />
+    <cms:embed '../../css/styles.css' />
+    <cms:embed '../../css/responsive.css' />
 
+</cms:abort>
 <?php COUCH::invoke(); ?>
